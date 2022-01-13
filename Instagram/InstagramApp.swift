bd7bcf292682cb7aa4249a8e29ef58127b5ff747
albertsalgueda <albert.salgueda01@estudiant.upf.edu>
2022-01-12 21:20:34 -0500

    Initial Commit

---
+++ Instagram/InstagramApp.swift
@@ -0,0 +1,17 @@
+//
+//  InstagramApp.swift
+//  Instagram
+//
+//  Created by MYX on 1/12/22.
+//
+
+import SwiftUI
+
+@main
+struct InstagramApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
